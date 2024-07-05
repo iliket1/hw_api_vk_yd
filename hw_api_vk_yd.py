9188e0f32d18c32ce97e3676c2b0d1677dfff28f
IliKet <Nika-katena@yandex.ru>
2024-07-05 18:56:42 +1100

    Update Homework API

--- hw_api_vk_yd.py
+++ hw_api_vk_yd.py
@@ -47,9 +47,7 @@ class VKClient:
          for photo in all_photo:
             file_time = dt.fromtimestamp(photo['date']).strftime('%Y_%m_%d')
             photo_info_dct = {}
-            # не пойму, как здесь реализовать добавление даты к имени файла с повторяющимся количеством лайков
-            photo_info_dct['date'] = file_time
-            photo_info_dct['file_name'] = f"{photo['likes']['count']}.jpg" 
+            photo_info_dct['file_name'] = f"{photo['likes']['count']}_{file_time}.jpg" 
             photo_info_dct['url'] = photo['sizes'][-1]['url']
             photo_info_dct['size'] = photo['sizes'][-1]['type']
             photo_info_lst.append(photo_info_dct)
@@ -59,40 +57,42 @@ class VKClient:
          return None
 
 class YDClient:
-   base_url_yd = 'https://cloud-api.yandex.net'
+   base_url_yd = 'https://cloud-api.yandex.net/v1/disk/'
 
    def __init__(self, yd_token):
       
       self.token = f'OAuth {yd_token}'
 
-   def create_folder_yd(self):
+   def create_folder_yd(self, folder_name):
 
-      params = {'path': 'image_vk'}
       headers = {'Authorization': self.token}
-      response = requests.put(f'{self.base_url_yd}/v1/disk/resources', params=params, headers=headers)
+      response = requests.put(f'{self.base_url_yd}resources?path={folder_name}', headers=headers)
       if 200 <= response.status_code < 300:
             print('Folder successfully created!')
-            return response.json()['href']
+            return response.json()
       else:
          print('Folder creation failed!')
          print(response.json()['description'])
          return False
       
    
-   
-   def download_images(self):
-      params = {'path': self.create_folder_yd()}
-      # photo_info_lst = VKClient.get_profile_photos() каким образом здесь настроить взаимодействие, нет понимания совсем
-      for photo_info in tqdm(photo_info_lst, desc='Downloading', unit='photo'):
-         url = photo_info['url']
-         response = requests.post(url, params=params)
-      # и что дальше, тоже затык
+   def download_images(self, file_url, file_name, folder_name):
+      headers = {'Authorization': self.token}
+      params = {'url': file_url, 'path': f'{folder_name}/{file_name}', 'fields': file_name, 'overwrite': 'true'}
+      response = requests.post(f'{self.base_url_yd}resources/upload', headers=headers, params=params)
+      return response.json()
 
 
 
 
 vk = VKClient(access_token, user_id)
 yd = YDClient(yd_token)
-# pprint(vk.get_profile_photos())
-print(yd.create_folder_yd())
-# print(yd.download_images())
+
+photos = vk.get_profile_photos()
+folder_name = 'vk_photos'
+yd.create_folder_yd(folder_name)
+
+for photo in tqdm(photos, desc='Uploading photos'):
+   file_url = photo['url']
+   file_name = photo['file_name']
+   yd.download_images(file_url, file_name, folder_name)
